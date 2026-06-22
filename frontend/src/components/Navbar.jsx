c1006b25425415523dea0c4942015bb60a97adf9
mk367621 <mk367621@gmail.com>
2026-06-22 17:17:05 +0530

    Style navbar with Tailwind

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,15 +8,33 @@ function Navbar() {
     navigate("/login");
   }
   return (
-    <div>
-      <Link to="/login">Login</Link> {"  "}
-      <Link to="/signup">Sign up</Link>
-      {"  "}
-      <Link to="/courses">courses</Link>
-      {"   "}
-      <Link to="/purchases">purchases</Link> {"  "}
-      <button onClick={handleLogout}>Logout</button>
-    </div>
+    <nav className="flex justify-between items-center px-6 py-4 shadow-md bg-white">
+      <h1 className="text-blue-600 text-2xl font-bold">Coursehub</h1>
+      <div className="flex gap-4 items-center">
+        <Link className="hover:text-blue-600 transition-colors" to="/login">
+          Login
+        </Link>
+
+        <Link className="hover:text-blue-600 transition-colors" to="/signup">
+          Sign up
+        </Link>
+
+        <Link className="hover:text-blue-600 transition-colors" to="/courses">
+          courses
+        </Link>
+
+        <Link className="hover:text-blue-600 transition-colors" to="/purchases">
+          purchases
+        </Link>
+
+        <button
+          className="bg-red-500 px-3 py-1 text-white hover:bg-red-600 rounded-md"
+          onClick={handleLogout}
+        >
+          Logout
+        </button>
+      </div>
+    </nav>
   );
 }
 
